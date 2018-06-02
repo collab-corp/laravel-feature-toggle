ab1b9671634da92922255beb1e4d487ad6fdf06c
sasin91 <jonas.kerwin.hansen@gmail.com>
2018-06-02 16:04:44 +0200

    default config

---
+++ config/features.php
@@ -0,0 +1,17 @@
+<?php return [
+    /*
+    |--------------------------------------------------------------------------
+    | Feature toggles
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the feature toggles in your system.
+    |
+    | A feature toggle should point at a boolean or string value.
+    | 
+    | if you need to evaluate something, you may refer to a bound feature as a string.
+    | @link https://github.com/collab-corp/laravel-feature-toggle#binding_callbacks
+    | 
+    | You may refer to nested feature toggles using the dot syntax.
+    */
+    // 'my_feature' => false
+];
